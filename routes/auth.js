28892b014a0475b56f9c0198b1b4400b03fa7a53
Raphael Gomes <radago@hotmail.com.br>
2019-10-22 11:33:18 -0300

    Adição da página admin

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = require("../models/user");
 Router.use((req, res, next) => {
   if ("user" in req.session) {
     res.locals.user = req.session.user;
+    res.locals.role = req.session.role;
   }
   next();
 });
@@ -14,6 +15,13 @@ Router.get("/logout", async (req, res) => {
   await req.session.destroy();
   res.redirect("/");
 });
+Router.get("/change-role/:role", async (req, res) => {
+  if ("user" in req.session) {
+    if (req.session.user.roles.indexOf(req.params.role) >= 0)
+      req.session.role = req.params.role;
+  }
+  res.redirect("/");
+});
 
 Router.post("/login", async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
@@ -21,6 +29,7 @@ Router.post("/login", async (req, res) => {
     const isValid = await user.checkPassword(req.body.password);
     if (isValid) {
       req.session.user = user;
+      req.session.role = user.roles[0];
       res.redirect("/restrict/noticias");
     }
   } else res.redirect("/login");
