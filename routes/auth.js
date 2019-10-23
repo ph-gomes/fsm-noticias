f02a1c140b8ea7da9223d7b2388f63e8b6b9926e
Raphael Gomes <radago@hotmail.com.br>
2019-10-23 11:37:40 -0300

    Login pelo Facebook

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const Router = require("express").Router();
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const FacebookStrategy = require("passport-facebook");
 
 const User = require("../models/user");
 
@@ -14,6 +15,30 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
+
+/** Definindo a estrategia para login com o FB */
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "1165285843675125",
+      clientSecret: "1842d4875867c182a3801ce8b76e62d2",
+      callbackURL: "http://localhost:3000/facebook/callback",
+      profileFields: ["id", "displayName", "email", "photos"]
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const userDB = await User.findOne({ facebookId: profile.id });
+      if (!userDB) {
+        const user = new User({
+          name: profile.displayName,
+          facebookId: profile.id,
+          roles: ["restrito"]
+        });
+        await user.save();
+        done(null, user);
+      } else done(null, userDB);
+    }
+  )
+);
 
 // definindo a estrategia para login local
 passport.use(
@@ -47,6 +72,12 @@ Router.get("/change-role/:role", async (req, res) => {
   }
   res.redirect("/");
 });
+Router.get("/facebook", passport.authenticate("facebook"));
+Router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/")
+);
 
 Router.post(
   "/login",
